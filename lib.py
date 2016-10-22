a2312794bdb95aede8640a9f30a146c848b5a851
paulivan <polivan69@gmail.com>
2016-10-22 19:34:45 +0200

    initialisation ok

--- lib.py
+++ lib.py
@@ -1,8 +1,15 @@
 import  math, random, time
 import vrep
 
+def loadScene():
+    vrep.simxCloseScene(0, vrep.simx_opmode_oneshot)
+    print ("loading scene")
+    if vrep.simxLoadScene(0, "/home/tutu/projects/ia/2wheel1arm/test/2w1a.ttt", 0, vrep.simx_opmode_blocking) < 0:
+        raise Exception("cannot load scene")
+
+
 def getPosition(roboth):
-    ret, robotPos = vrep.simxGetObjectPosition(0, roboth, -1, vrep.simx_opmode_streaming)           
+    ret, robotPos = vrep.simxGetObjectPosition(0, roboth, -1, vrep.simx_opmode_blocking)           
     return robotPos
 
 def setPosition(roboth, pos):
@@ -13,6 +20,7 @@ def setRotation(handle, angle):
 
 def connect():
     idc = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)  
+    print("client is %s" %idc)
     if (idc < 0):
         raise Exception("Fail to connect")
     print("connected")
@@ -28,13 +36,19 @@ def animate(robot):
     pass
 
 def getHandle(name):
-    ret, item = vrep.simxGetObjectHandle(0, name, vrep.simx_opmode_oneshot_wait)
+    ret, item = vrep.simxGetObjectHandle(0, name, vrep.simx_opmode_blocking)
 
     if ret < 0:
         raise Exception("cant retrive item" + str(name))
     return item
 
 def copyRobot(id_robot, num):
-    ret, item = vrep.simxCopyPasteObjects(0, id_robot, vrep.simx_opmode_oneshot_wait)
+    ret, item = vrep.simxCopyPasteObjects(0, [id_robot], vrep.simx_opmode_blocking)
+    print(item)
+    wrist = getHandle("WristMotor#" + str(num)) 
+    elbow = getHandle("ElbowMotor#" + str(num))
+    shoulder = getHandle("ShoulderMotor#" + str(num))
 
     return item[0], wrist, elbow, shoulder
+
+
--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3 -i
 
-import vrep, math, random, time, lib as ml
+import vrep, math, random, time, copy, lib as ml
 
 NB_GENERATIONS = 300
+NB_ROBOTS = 2
 
 
+#Initialisation
 
 vrep.simxFinish(-1)
 ml.connect()
-
-
+ml.loadScene()
 wrist = ml.getHandle("WristMotor")
 elbow = ml.getHandle("ElbowMotor")
 shoulder = ml.getHandle("Shoulder")
 robot = ml.getHandle("2W1A")
 
+rbtLst = [[robot, wrist, elbow, shoulder]]
+initPos = ml.getPosition(robot)
+print(initPos)
+
 for i in range(0, NB_ROBOTS):
-    wrist, elbow, shoulder = coyRobot(robot)
+    new_rb = ml.copyRobot(robot, i)
+    rbtLst.append(new_rb)
+    pos = [initPos[0], 0.5 + initPos[1] + (0.5 * i), initPos[2]]
+    ml.setPosition(new_rb[0], pos)
+
 
 print("starting")
+print ("robot %s wrist %s elbow %s shoulder %s" % (robot, wrist, elbow, shoulder))
